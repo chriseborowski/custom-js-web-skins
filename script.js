f9e60d22a0ba59cc695383d61290d59e66768c31
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-22 16:44:42 -0500

    Initial script.js version

---
+++ script.js
@@ -0,0 +1,120 @@
+document.getElementById('arsenal').onclick = paintArsenal
+document.getElementById('aston-villa').onclick = paintAstonVilla
+document.getElementById('bournemouth').onclick = paintBournemouth
+document.getElementById('brighton').onclick = paintBrighton
+document.getElementById('brentford').onclick = paintBrentford
+document.getElementById('burnley').onclick = paintBurnley
+document.getElementById('chelsea').onclick = paintChelsea
+document.getElementById('crystal-palace').onclick = paintCrystalPalace
+document.getElementById('everton').onclick = paintEverton
+document.getElementById('fulham').onclick = paintFulham
+document.getElementById('liverpool').onclick = paintLiverpool
+document.getElementById('luton').onclick = paintLuton
+document.getElementById('manchester-city').onclick = paintManchesterCity
+document.getElementById('manchester-united').onclick = paintManchesterUnited
+document.getElementById('newcastle').onclick = paintNewcastle
+document.getElementById('nottingham-forest').onclick = paintNottinghamForest
+document.getElementById('sheffield-united').onclick = paintSheffieldUnited
+document.getElementById('tottenham').onclick = paintTottenham
+document.getElementById('west-ham').onclick = paintWestHam
+document.getElementById('wolverhampton').onclick = paintWolverhampton
+
+function paintArsenal() {
+  document.querySelector('body').style.backgroundColor = 'rgb(239, 1, 7)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintAstonVilla() {
+  document.querySelector('body').style.backgroundColor = 'rgb(149, 191, 229)'
+  document.querySelector('body').style.color = 'rgb(103, 14, 54)'
+}
+
+function paintBournemouth() {
+  document.querySelector('body').style.backgroundColor = 'rgb(218, 41, 28)'
+  document.querySelector('body').style.color = 'rgb(0, 0, 0)'
+}
+
+function paintBrighton() {
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 87, 184)'
+  document.querySelector('body').style.color = 'rgb(255, 205, 0)'
+}
+
+function paintBrentford() {
+  document.querySelector('body').style.backgroundColor = 'rgb(193, 0, 0)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintBurnley() {
+  document.querySelector('body').style.backgroundColor = 'rgb(108, 29, 69)'
+  document.querySelector('body').style.color = 'rgb(153, 214, 234)'
+}
+
+function paintChelsea() {
+  document.querySelector('body').style.backgroundColor = 'rgb(3, 70, 148)'
+  document.querySelector('body').style.color = 'rgb(209, 211, 212)'
+}
+
+function paintCrystalPalace() {
+  document.querySelector('body').style.backgroundColor = 'rgb(27, 69, 143)'
+  document.querySelector('body').style.color = 'rgb(167, 165, 166)'
+}
+
+function paintEverton() {
+  document.querySelector('body').style.backgroundColor = 'rgb(39, 68, 136)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintFulham() {
+  document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintLiverpool() {
+  document.querySelector('body').style.backgroundColor = 'rgb(200, 16, 46)'
+  document.querySelector('body').style.color = 'rgb(246, 235, 97)'
+}
+
+function paintLuton() {
+  document.querySelector('body').style.backgroundColor = 'rgb(247, 143, 30)'
+  document.querySelector('body').style.color = 'rgb(0, 0, 0)'
+}
+
+function paintManchesterCity() {
+  document.querySelector('body').style.backgroundColor = 'rgb(108, 171, 221)'
+  document.querySelector('body').style.color = 'rgb(28, 44, 91)'
+}
+
+function paintManchesterUnited() {
+  document.querySelector('body').style.backgroundColor = 'rgb(218, 41, 28)'
+  document.querySelector('body').style.color = 'rgb(251, 225, 34)'
+}
+
+function paintNewcastle() {
+  document.querySelector('body').style.backgroundColor = 'rgb(45, 41, 38)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintNottinghamForest() {
+  document.querySelector('body').style.backgroundColor = 'rgb(221, 0, 0)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintSheffieldUnited() {
+  document.querySelector('body').style.backgroundColor = 'rgb(238, 39, 55)'
+  document.querySelector('body').style.color = 'rgb(0, 0, 0)'
+}
+
+function paintTottenham() {
+  document.querySelector('body').style.backgroundColor = 'rgb(19, 34, 87)'
+  document.querySelector('body').style.color = 'rgb(255, 255, 255)'
+}
+
+function paintWestHam() {
+  document.querySelector('body').style.backgroundColor = 'rgb(122, 38, 58)'
+  document.querySelector('body').style.color = 'rgb(27, 177, 231)'
+}
+
+function paintWolverhampton() {
+  document.querySelector('body').style.backgroundColor = 'rgb(253, 185, 19)'
+  document.querySelector('body').style.color = 'rgb(35, 31, 32)'
+}
